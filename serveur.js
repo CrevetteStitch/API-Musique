f31cc579fa4f4dcfa398216437a51f4e86f55c60
CrevetteStitch <theo@vodmail.fr>
2024-05-02 10:36:37 +0200

    cors

--- serveur.js
+++ serveur.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json'); // Importez votre fichier JSON Swagger ici
 const port = 3000;
@@ -6,12 +7,14 @@ const version = 'v1';
 const router = require('./routes/routes');
 const app = express();
 
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(`/api/${version}`, router);
 
-// Middleware pour afficher la documentation Swagger
+
 app.use(`/api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
